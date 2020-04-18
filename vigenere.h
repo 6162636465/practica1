8464a91d0e7312d1b41005788dfdca5f978cb4f6
javier.alvarez Reyes <40539959+6162636465@users.noreply.github.com>
2020-04-18 08:09:26 -0500

    Update vigenere.h
    actualización del archivo ,se corrigió la encriptacion y ahora se le agrego la descencriptacion todo el código se iso para que solo el alfabeto se suba por .txt

--- vigenere.h
+++ vigenere.h
@@ -18,10 +18,6 @@ public:
   cifrado(int,string,string,string);
   void cifrar();
   void decifrar();
-  void alfabetoASCII();
-  void alfabetoHEX();
-  void alfabetoNUMERICO();
-
 };
 cifrado::cifrado(int _x,string _m,string _n,string _sifrado)
 {
@@ -30,21 +26,105 @@ cifrado::cifrado(int _x,string _m,string _n,string _sifrado)
   m=_m;//clave
   sifrado=_sifrado;
 }
-void cifrado::alfabetoASCII()
+void cifrado::cifrar()
 {
   string alfabeto;
   string espacio(" ");
   ifstream archivo;
-  archivo.open("alfabetoACII.txt",ios::in);
   int j=0,u=0,F=0,c=0,q=0;
-  while(!archivo.eof())
-    {
-       getline(archivo,alfabeto);
-    }
-  archivo.close();
-  if(alfabeto[0]!=espacio[0])
+  if(x==1)
   {
-    alfabeto[0]=espacio[0];
+    archivo.open("alfabetoACII.txt",ios::in);
+    while(!archivo.eof())
+      {
+        getline(archivo,alfabeto);
+      }
+    archivo.close();
+  }
+  else if(x==2)
+  {
+    archivo.open("alfabetoWord.txt",ios::in);
+    while(!archivo.eof())
+      {
+        getline(archivo,alfabeto);
+      }
+    archivo.close();
+  }
+  else if(x==3)
+  {
+  }
+  int b=0;
+  while(m.length()<n.length())//completar la clave
+  {
+    m=m+m[j];
+    j+=1;
+  }
+  cout<<m<<endl;
+  for(int i=0;i<m.length();i++)
+  {
+    for(int k=0;k<alfabeto.length();k++)
+    {
+      if(m[i]==alfabeto[k] && n[i]==alfabeto[k])
+      {
+        F=k;
+        u=k;
+        cout<<"EL VALOR Dela respuesta"<<k<<k<<endl;
+      }
+      else if(m[i]==alfabeto[k] && n[i]!=alfabeto[k])
+      {
+        F=k;
+        cout<<"EL VALOR Dela respuesta"<<k<<endl;
+      }
+      else if(n[i]==alfabeto[k] && m[i]!=alfabeto[k])
+      {
+        u=k;
+        cout<<"EL VALOR Del mensaje"<<k<<endl;
+      }
+    }
+    c=F+u;
+    if(alfabeto.length()<=c)
+    {
+      c=c-alfabeto.length();
+      cout<<"ENTRO"<<endl;
+    }
+    cout<<alfabeto.length()<<endl;
+    cout<<c<<endl;
+    cout<<i<<endl;
+    cout<<alfabeto[c]<<endl;
+    sifrado=sifrado+alfabeto[c];
+  }
+  cout<<sifrado<<endl;
+}
+void cifrado::decifrar()
+{
+  string alfabeto;
+  string espacio(" ");
+  ifstream archivo;
+  int j=0,u=0,F=0,c=0,q=0;
+  if(x==1)
+  {
+    archivo.open("alfabetoACII.txt",ios::in);
+    while(!archivo.eof())
+      {
+        getline(archivo,alfabeto);
+      }
+    archivo.close();
+    if(alfabeto[0]!=espacio[0])
+      {
+      alfabeto[0]=espacio[0];
+      }
+  }
+  else if(x==2)
+  {
+    archivo.open("alfabetoWord.txt",ios::in);
+    while(!archivo.eof())
+      {
+        getline(archivo,alfabeto);
+      }
+    archivo.close();
+  }
+  else if(x==3)
+  {
   }
   int b=0;
   while(m.length()<n.length())//completar la clave
@@ -56,42 +136,29 @@ void cifrado::alfabetoASCII()
   {
     for(int k=0;k<alfabeto.length();k++)
     {
-      if(m[i]==alfabeto[k])
+      if(m[i]==alfabeto[k] && n[i]==alfabeto[k])
       {
         F=k;
+        u=k;
+        cout<<"EL VALOR Dela respuesta"<<k<<k<<endl;
       }
-      else if(n[i]==alfabeto[k])
+      else if(m[i]==alfabeto[k] && n[i]!=alfabeto[k])
+      {
+        F=k;
+        cout<<"EL VALOR Dela respuesta"<<k<<endl;
+      }
+      else if(n[i]==alfabeto[k] && m[i]!=alfabeto[k])
       {
         u=k;
+        cout<<"EL VALOR Del mensaje"<<k<<endl;
       }
     }
-    c=F+u;
-    
-    if(alfabeto.length()<c)
+    c=u-F;
+    if(c<0)
     {
-      c=c-alfabeto.length();
+      c=c+alfabeto.length();
     }
     sifrado=sifrado+alfabeto[c];
   }
   cout<<sifrado<<endl;
-}
-void cifrado::alfabetoHEX()
-{
-
-}
-void cifrado::alfabetoNUMERICO()
-{
-
-}
-void cifrado::cifrar()
-{
-  if(x==1){
-  alfabetoASCII();
-  }
-  else if(x==2){
-  alfabetoHEX();
-  }
-  else if(x==3){
-  alfabetoNUMERICO();
-  }
 }
